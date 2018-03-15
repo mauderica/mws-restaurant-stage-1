dd1250acb7de496d2a077ff2199a8183e93b61c8
mauderica <maude.chalin@gmail.com>
2018-03-15 11:32:36 -0400

    Accessibility (#5)
    * feat(images): add descriptive alternate text
    
    * feat: manage focus

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,6 +154,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image alt text.
+   */
+  static imageAltTextForRestaurant(restaurant) {
+    return restaurant.altText;
+  }
+
+  /**
    * Map marker for a restaurant.
    */
   static mapMarkerForRestaurant(restaurant, map) {
--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img cover';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = DBHelper.imageAltTextForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
